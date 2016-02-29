c72616bd7e5078eff65babed00739c7f25fcc94c
Andy Morgan <asmorgan@student.ysu.edu>
2016-02-28 21:59:52 -0500

    hello

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,6 +15,21 @@
 #include <fstream>
 using namespace std;
 
+int convertEndian(char C[], int size){
+    int  j = 0;
+    char temp [size] ;
+    for (int i = size-1; i>=0; i--){
+        C[i] = temp[j];
+        j++;
+
+        cout << hex << setw(2) << setfill('0') << (int)temp[j] << " ";
+    }
+    cout << endl;
+
+    return 0;
+
+}
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -43,7 +58,6 @@ void MainWindow::on_browseVDIPushButton_clicked()
 //
     string fileString = fileName.toStdString();
 
-    cout << fileString << endl;
     char *fileChar = new char[fileString.length() + 1];
 
     std::strcpy(fileChar, fileString.c_str());
@@ -53,68 +67,141 @@ void MainWindow::on_browseVDIPushButton_clicked()
     input.open(fileChar,std::ios::in);
 
 
-    if(!input.is_open())
-        cout << "tru" << endl;
+
     input >> noskipws;
 
 
-    //
-    // Put in getHeader Method
-    //---------------------------------------------------
-    //Size of Header
-    unsigned char header_size[4];
-    input.seekg(72);
-    for (int i=0; i<4;i++){
-        input >> header_size[i];
-        cout << hex << setw(2) << setfill('0') << (int)header_size[i] << " ";
-    }
-    cout << endl;
+            //
+            // Put in getHeader Method
+            //---------------------------------------------------
+            //Size of Header
+            unsigned char header_size[4];
+            input.seekg(72);
+            for (int i=0; i<4;i++){
+                input >> header_size[i];
+                //cout << hex << setw(2) << setfill('0') << (int)header_size[i] << " ";
+                int size_of_header=convertEndian(header_size[4],4);
+            }
+            //cout << endl;
 
-    //---------------------------------------------
-    //Image Type
-    unsigned char image_type[4];
-    input.seekg(76);
-    for (int i=0; i<4;i++){
-        input >> image_type[i];
-        cout << hex << setw(2) << setfill('0') << (int)image_type[i] << " ";
-    }
-    cout << endl;
+            //---------------------------------------------
+            //Image Type
+            unsigned char image_type[4];
+            input.seekg(76);
+            for (int i=0; i<4;i++){
+                input >> image_type[i];
+                //cout << hex << setw(2) << setfill('0') << (int)image_type[i] << " ";
+            }
+            //cout << endl;
 
-    //---------------------------------------------
-    //Offset Blocks
-    unsigned char offsetBlocks[4];
-    input.seekg(340);
-    for (int i=0; i<4;i++){
-        input >> offsetBlocks[i];
-        cout << hex << setw(2) << setfill('0') << (int)offsetBlocks[i] << " ";
-    }
-    cout << endl;
-
-
-    //---------------------------------------------
-    //Offset Data
-    unsigned char offsetData[4];
-    input.seekg(344);
-    for (int i=0; i<4;i++){
-        input >> offsetData[i];
-        cout << hex << setw(2) << setfill('0') << (int)offsetData[i] << " ";
-    }
-    cout << endl;
-
-    //---------------------------------------------
-    //Offset Data
-    unsigned char sectorSize[4];
-    input.seekg(360);
-    for (int i=0; i<4;i++){
-        input >> sectorSize[i];
-        cout << hex << setw(2) << setfill('0') << (int)sectorSize[i] << " ";
-    }
-    cout << endl;
+            //---------------------------------------------
+            //Offset Blocks
+            unsigned char offsetBlocks[4];
+            input.seekg(340);
+            for (int i=0; i<4;i++){
+                input >> offsetBlocks[i];
+               // cout << hex << setw(2) << setfill('0') << (int)offsetBlocks[i] << " ";
+            }
+            //cout << endl;
 
 
+            //---------------------------------------------
+            //Offset Data
+            unsigned char offsetData[4];
+            input.seekg(344);
+            for (int i=0; i<4;i++){
+                input >> offsetData[i];
+              //  cout << hex << setw(2) << setfill('0') << (int)offsetData[i] << " ";
+            }
+            // cout << endl;
+
+            //---------------------------------------------
+            //Sector Size
+            unsigned char sectorSize[4];
+            input.seekg(360);
+            for (int i=0; i<4;i++){
+                input >> sectorSize[i];
+              //  cout << hex << setw(2) << setfill('0') << (int)sectorSize[i] << " ";
+            }
+            // cout << endl;
+
+            //---------------------------------------------
+            //Disk Size (Bytes)
+            unsigned char discSize[8];
+            input.seekg(368);
+            for (int i=0; i<8;i++){
+                input >> discSize[i];
+              //  cout << hex << setw(2) << setfill('0') << (int)discSize[i] << " ";
+            }
+            // cout << endl;
+
+            //---------------------------------------------
+            //Block Size
+            unsigned char blockSize[4];
+            input.seekg(376);
+            for (int i=0; i<4;i++){
+                input >> blockSize[i];
+            //    cout << hex << setw(2) << setfill('0') << (int)blockSize[i] << " ";
+            }
+            //cout << endl;
+
+            //---------------------------------------------
+            //Blocks in HDD
+            unsigned char blocksInHDD[4];
+            input.seekg(384);
+            for (int i=0; i<4;i++){
+                input >> blocksInHDD[i];
+             //   cout << hex << setw(2) << setfill('0') << (int)blocksInHDD[i] << " ";
+            }
+            // cout << endl;
+
+            //---------------------------------------------
+            //Blocks Allocated
+            unsigned char blocksAllocated[4];
+            input.seekg(388);
+            for (int i=0; i<4;i++){
+                input >> blocksAllocated[i];
+             //   cout << hex << setw(2) << setfill('0') << (int)blocksAllocated[i] << " ";
+            }
+            //cout << endl;
+
+            //---------------------------------------------
+            //UUID of this VDI
+            unsigned char UUIDofVDI[16];
+            input.seekg(392);
+            for (int i=0; i<16;i++){
+                input >> UUIDofVDI[i];
+             //   cout << hex << setw(2) << setfill('0') << (int)UUIDofVDI[i] << " ";
+            }
+            //cout << endl;
+
+
+            //---------------------------------------------
+            //UUID of last SNAP
+            unsigned char UUIDofSNAP[16];
+            input.seekg(408);
+            for (int i=0; i<16;i++){
+                input >> UUIDofSNAP[i];
+              //  cout << hex << setw(2) << setfill('0') << (int)UUIDofSNAP[i] << " ";
+            }
+            //cout << endl;
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
